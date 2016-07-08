8e1502a1304565c88096ee91d127ee750769b1d1
Yassine Rafa <yassine.rafa@outlook.com>
2016-07-08 17:45:59 +0000

    Finish to setup all the document controller action and views

--- app/controllers/documents_controller.rb
+++ app/controllers/documents_controller.rb
@@ -1,2 +1,49 @@
 class DocumentsController < ApplicationController
+  before_action :find_document, only: [:show, :edit, :update, :destroy]
+
+  def index
+    @document = Document.all.order("created_at DESC")
+  end
+
+  def new
+    @document = Document.new
+  end
+
+  def show
+  end
+
+  def create
+    @document = Document.new(doc_params)
+    if @document.save
+      redirect_to @document
+    else
+      render 'new'
+    end
+  end
+
+  def edit
+  end
+
+  def update
+    if @document.update(doc_params)
+      redirect_to @document
+    else
+      render 'edit'
+    end
+  end
+
+  def destroy
+    @document.destroy
+    redirect_to documents_path
+  end
+
+  private
+
+    def find_document
+      @document = Document.find(params[:id])
+    end
+
+    def doc_params
+      params.require(:document).permit(:title, :content)
+    end
 end
